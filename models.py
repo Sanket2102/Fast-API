7bded5e855a7e4f70ca046f81a936ce832610592
Sanket Anand <asanket318@gmail.com>
2024-12-02 14:06:38 +0530

    Updated columns

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
 from database import Base
 
 class Post(Base):
@@ -7,5 +9,5 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     title = Column(String(255), nullable=False)
     content = Column(String(5000), nullable=False)
-    published = Column(Boolean, default=True)
-
+    published = Column(Boolean, server_default="1", nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
