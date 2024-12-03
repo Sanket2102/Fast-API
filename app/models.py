e0a6f321f591e176096dccc21ce0cbb9b181ce2a
Sanket Anand <asanket318@gmail.com>
2024-12-03 10:42:33 +0530

    Updated password column to store long hashed passwords

--- app/models.py
+++ app/models.py
@@ -17,5 +17,5 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     email = Column(String(25), nullable=False, unique=True)
-    password = Column(String(30), nullable=False)
+    password = Column(String(300), nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
