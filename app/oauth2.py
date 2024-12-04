ab0e83ea17988ade4a0dc194b150068492e62b7f
Sanket Anand <asanket318@gmail.com>
2024-12-05 01:46:41 +0530

    Imported database to return user information

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,10 +1,11 @@
 from jose import jwt, JWTError
 from datetime import datetime, timedelta
-import app.schemas as schemas
+import app.schemas as schemas, app.database as database, app.models as models
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from dotenv import load_dotenv
 import os
+from sqlalchemy.orm import Session
 
 load_dotenv()
 
@@ -37,7 +38,10 @@ def verify_access_token(token: str, credentails_exception):
     
     return token_data
     
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
     credentails_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
 
-    return verify_access_token(token, credentails_exception)
+    token = verify_access_token(token, credentails_exception)
+    user = db.query(models.User).filter(models.User.id == token.id).first()
+
+    return user
