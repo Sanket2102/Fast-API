12fae21931930284ded64c007d62e5a005443c02
Sanket Anand <asanket318@gmail.com>
2024-12-05 03:00:48 +0530

    relationship between Post and User classes

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
 from app.database import Base
 
@@ -12,6 +13,8 @@ class Post(Base):
     published = Column(Boolean, server_default="1", nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
+    owner = relationship("User")
 
 class User(Base):
     __tablename__ = "users"
