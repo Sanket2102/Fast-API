2cd76200ade05501950628592698413a48309cc6
Sanket Anand <asanket318@gmail.com>
2024-12-05 02:42:50 +0530

    Added a logic so that user can only delete and update his own posts

--- app/routers/post.py
+++ app/routers/post.py
@@ -23,7 +23,7 @@ def fetch_post(id: int, db: Session = Depends(get_db)):
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
 def create_post(post: schemas.PostCreate, db:Session = Depends(get_db), current_user:int = Depends(oauth2.get_current_user)):
-    new_post = models.Post(**post.dict())
+    new_post = models.Post(user_id=current_user.id,**post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
@@ -32,10 +32,17 @@ def create_post(post: schemas.PostCreate, db:Session = Depends(get_db), current_
 
 @router.delete("/uid/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_posts(id:int, db: Session = Depends(get_db), current_user:int = Depends(oauth2.get_current_user)):
-    post = db.query(models.Post).filter(models.Post.id == id)
-    if not post.first():
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+
+    post = post_query.first()
+
+    if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No post found with the given id")
-    post.delete(synchronize_session=False)
+    
+    if post.user_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action") 
+    
+    post_query.delete(synchronize_session=False)
     db.commit()
 
 @router.put("/uid/{id}", response_model=schemas.PostResponse)
@@ -44,6 +51,9 @@ def update_post(post:schemas.PostCreate, id: int, db: Session = Depends(get_db),
     requested_post = post_query.first()
     if not requested_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No post found with the given id")
+    
+    if requested_post.user_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action") 
     
     post_query.update(post.dict())
     db.commit()
