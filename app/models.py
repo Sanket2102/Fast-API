06d8969206952c596f04ba0344fc805f38538480
Sanket Anand <asanket318@gmail.com>
2024-12-05 01:57:38 +0530

    Added a column user_id in the posts table

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
 from app.database import Base
@@ -11,6 +11,7 @@ class Post(Base):
     content = Column(String(5000), nullable=False)
     published = Column(Boolean, server_default="1", nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
 
 class User(Base):
     __tablename__ = "users"
